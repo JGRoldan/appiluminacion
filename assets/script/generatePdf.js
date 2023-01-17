c4ee2ec0f53cb4fa158e6ab4ddf828fa924b5425
Gaston Roldan <71336562+JGRoldan@users.noreply.github.com>
2023-01-17 13:30:53 -0300

    Update generatePdf.js

--- assets/script/generatePdf.js
+++ assets/script/generatePdf.js
@@ -1,29 +1,20 @@
-$(document).ready(function(){
-      
-    $('#generatePDF').click(function(){
-      $(".modal").attr("style", "display:flex");
-      setTimeout(function(){ $(".modal").attr("style", "display:none"); }, 1500);
-      $('#contenidoImprimir').printThis();
-    })
-})
-
 //Doesn't work in ANDROID because it doesn't have a print() native method 
 
-// const button = document.getElementById("generatePDF");
+const button = document.getElementById("generatePDF");
 
-// button.addEventListener("click", e=> {
-//     e.stopPropagation()
-//     const hide = document.querySelector(".botones");
-//     hide.style.display='none'
-//     const pantalla = document.querySelector(".modal");
-//     pantalla.style.display='flex'
-//     setTimeout(() => {
-//         pantalla.style.display='none'
-//     }, 1500);
+button.addEventListener("click", e=> {
+    e.stopPropagation()
+    const hide = document.querySelector(".botones");
+    hide.style.display='none'
+    const pantalla = document.querySelector(".modal");
+    pantalla.style.display='flex'
+    setTimeout(() => {
+        pantalla.style.display='none'
+    }, 1500);
     
-//     setTimeout(() => {
-//         document.getElementById('body').style.background = 'none'
-//         window.print()
-//         window.location.reload()
-//     },1800);
-// });
+    setTimeout(() => {
+        document.getElementById('body').style.background = 'none'
+        window.print()
+        window.location.reload()
+    },1800);
+});
